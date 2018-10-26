298654ae562310b4184f7d978c9f40a86a9b5bb6
Mustafa Alroomi <mustafa.alroomi.91@gmail.com>
2018-10-26 14:02:11 +0200

    simple node app with test

---
+++ test/test.js
@@ -0,0 +1,10 @@
+var request = require("supertest")
+var app = require("../index")
+describe("GET / ", function() {
+  it("‘respond with hello world’", function(done) {
+    //navigate to root and check the the response is "hello world"
+    request(app)
+      .get("/")
+      .expect("Hello world!!!!", done)
+  })
+})
